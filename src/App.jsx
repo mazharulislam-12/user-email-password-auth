15a8c8815fee961ebde79fea18796379ca58b033
mazharulislam-12 <505mazharulislam@gmail.com>
2023-09-28 22:12:58 -0700

    basic file setup and route setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h3>This is h2 tag</h3>
+      
+      
+    </>
+  )
+}
+
+export default App
