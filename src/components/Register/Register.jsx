36943aaecf1ee748df4dd8efab28451f7e0bfecd
mazharulislam-12 <505mazharulislam@gmail.com>
2023-09-29 03:23:23 -0700

    hero register form create

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,26 @@
 
 const Register = () => {
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password)
+    }
+
     return (
-        <div>
-            <h3 className="text-3xl">Please Register</h3>
+        <div className="mt-10">
+            <div className="mx-auto md:w-1/2">
+                <h3 className="text-3xl mb-4">Please Register</h3>
+                <form onSubmit={handleRegister}>
+                    <input className="mb-4 w-3/4 p-2 rounded-lg" type="email" name="email" placeholder="Email Address" id="" />
+                <br />
+                    <input className="mb-4 w-3/4 p-2 rounded-lg"  type="password" name="password" placeholder="Password" id="" />
+                <br />
+                    <input className="mb-4 w-3/4 btn btn-secondary p-2 rounded-lg"  type="submit" value="Register" />
+                </form>
+            </div>
+
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Root from './components/Root/Root';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import HeroRegister from './components/HeroRegister/HeroRegister';
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
         {
           path: '/register',
           element: <Register></Register>
+        },
+        {
+          path: '/registerHero',
+          element: <HeroRegister></HeroRegister>
         }
       ]
   },
